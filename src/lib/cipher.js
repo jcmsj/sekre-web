b6cb579f24258a1ff235897bba457997727547d9
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-10-27 16:15:16 +0800

    Feat: add SHA256

--- src/lib/cipher.js
+++ src/lib/cipher.js
@@ -1,4 +1,4 @@
-import { AES, enc } from "crypto-js"
+import { AES, enc, SHA256 } from "crypto-js"
 
 /**
  * @typedef {{name:string, secret:string}} Credential
@@ -8,3 +8,8 @@ import { AES, enc } from "crypto-js"
 
 export const decrypt = cipher => key => AES.decrypt(cipher, key).toString(enc.Utf8);
 export const encrypt = secret => key => AES.encrypt(secret, key).toString();
+
+/**
+ * @param {string} key 
+ */
+ export const obscure = key => SHA256(key).toString();
