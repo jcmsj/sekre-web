a51965d05744b62a1d6e029f4ee416aba423b07f
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-10-28 08:00:48 +0800

    Feat: Login Limiter

--- src/routes/LoginPage.jsx
+++ src/routes/LoginPage.jsx
@@ -1,19 +1,38 @@
 import { Button } from "@mui/material";
+import { useEffect } from "react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { InputOutline } from "../components/InputOutline";
 import TopBar from "../components/TopBar";
 import { useMainKey } from "../db";
 import { obscure } from "../lib/cipher";
+const MAX_ATTEMPTS = 5;
 export function LoginForm({ setAuth }) {
     const first = useMainKey()
+    const [attemptsLeft, setAttemptsLeft] = useState(MAX_ATTEMPTS);
+    const decrease = () => setAttemptsLeft(attemptsLeft - 1);
     async function authenticate(key) {
-        setAuth(obscure(key) == first.secret)
+        const result = obscure(key) == first.secret
+        setAuth(result)
+        result ? setAuth(result) :decrease()
     }
+
+    useEffect(() => {
+        if (attemptsLeft <= 0) {
+            alert("Max attempts reached. Exiting the app!")
+            //Replace with persistent timer?
+            window.close()
+        }
+    }, [attemptsLeft])
     return <AuthForm
         intent="Login"
         onSubmit={authenticate}
+        siblingsAfter={
+            <p style={{color:"red"}}>
+            {attemptsLeft != MAX_ATTEMPTS ? "Incorrect key, please try again!":""}
+            </p>
+        }
     >
+        Attempts left: {attemptsLeft}
     </AuthForm>;
 }
 
@@ -21,12 +40,18 @@ export function LoginForm({ setAuth }) {
  * 
  * @param {{
  * intent:string,
- * onSubmit:(key:string)=>void
+ * onSubmit:(key:string)=>void,
+ * siblingsBefore: React.ReactNode,
+ * siblingsAfter: React.ReactNode,
+ * children: React.ReactNode,
+ * inputProps: import("@mui/material").TextFieldProps
  * }} param0 
+ * @implNote {
+ *   siblingsBefore/after pertain to the input field
+ * }
  * @returns 
  */
-export function AuthForm({ intent, siblingsBefore, siblingsAfter, onSubmit, cancellable, ...props }) {
-    const navigate = useNavigate();
+export function AuthForm({ intent, siblingsBefore, siblingsAfter, onSubmit, cancellable, inputProps, children, ...props }) {
     const [input, setInput] = useState("")
     return <>
         <TopBar
@@ -48,7 +73,9 @@ export function AuthForm({ intent, siblingsBefore, siblingsAfter, onSubmit, canc
                 label="Key"
                 value={input}
                 onInput={e => setInput(e.target.value)}
+                {...inputProps}
             />
+            {siblingsAfter}
             <Button
                 variant="contained"
                 sx={{
@@ -57,7 +84,7 @@ export function AuthForm({ intent, siblingsBefore, siblingsAfter, onSubmit, canc
                 }}
                 onClick={e => onSubmit(input, e)}
             >Submit</Button>
-            {siblingsAfter}
+            {children}
         </div>
     </>
 }
