f3c362d236ed58892e2b2c6f328cc7f92dff6641
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-10-28 09:09:21 +0800

    WIP: Edit page UI and logic

--- src/routes/EditPage.jsx
+++ src/routes/EditPage.jsx
@@ -1,5 +1,6 @@
+import { ContentCopy, RemoveRedEye } from "@mui/icons-material";
+import { List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import { useLiveQuery } from "dexie-react-hooks";
-import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import TopBar from "../components/TopBar";
 import { db } from "../db";
@@ -8,19 +9,18 @@ import { db } from "../db";
  * @param {{sekre:import("./db").Sekre,key:string?}} param0 
  */
 export default function EditPage() {
-    const { id } = useParams();
-    const [sekre, setSekre] = useState();
     const navigate = useNavigate()
-    useEffect(() => {
-        async function check() {
-            const s = await db.secrets.get(parseInt(id))
-            setSekre(s)
-            if (s == undefined) {
-                navigate("/")
-            }
+    const { id:rawID } = useParams();
+    const id = parseInt(rawID)
+    const defaultKeyID = useLiveQuery(() => db.chains.where({targetID:id}).first());
+    const key = useLiveQuery(() => defaultKeyID ? db.mainKey.get({ id: defaultKeyID.keyID }):undefined)
+    const sekre = useLiveQuery(() => {
+        const s = db.secrets.get(id)
+        if (s == undefined) {
+            navigate("/");
         }
-        check()
-    }, [id])
+        return s;
+    });
 
     return <>
         <TopBar
@@ -32,5 +32,23 @@ export default function EditPage() {
         >
 
         </TopBar>
+        <List>
+            <ListItemButton>
+                <ListItemText>
+                    Reveal
+                </ListItemText>
+                <ListItemIcon>
+                    <RemoveRedEye />
+                </ListItemIcon>
+            </ListItemButton>
+            <ListItemButton>
+                <ListItemText>
+                    Copy
+                </ListItemText>
+                <ListItemIcon>
+                    <ContentCopy />
+                </ListItemIcon>
+            </ListItemButton>
+        </List>
     </>;
 }
