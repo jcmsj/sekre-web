dc1b1dc7f805bc0e12723bc6ebddadf2d87342ce
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-10-29 16:07:25 +0800

    Feat: Dev environment https. WIP: ProgressiveWebApp

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
-
+import mkcert from "vite-plugin-mkcert"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    splitVendorChunkPlugin(),
+    mkcert(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "sekre-web",
+        short_name: "sekre-web",
+        start_url: "/",
+        display: "standalone",
+        lang: "en",
+        scope: "/",
+        background_color: "white",
+        theme_color: "#FFFFFF",
+        icons: [
+
+        ]
+      }
+    }),
+    react()
+  ],
+}
+)
