79225f3ff59488c175065dc4d3ddb47336f3c4f6
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-10-29 13:43:50 +0800

    Fix: counter

--- src/lib/useCounter.jsx
+++ src/lib/useCounter.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export function useCounter(initialState) {
     const [count, setCount] = useState(initialState);
     const increment = (n = 1) => setCount(count + n);
-    const decrement = (n = -1) => setCount(count - n);
+    const decrement = (n = 1) => setCount(count - n);
     return {
         count,
         decrement,
