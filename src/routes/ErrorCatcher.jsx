f2e8521a099f33bb0bb13fb8a978df127e5302cb
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-10-30 15:02:24 +0800

    Feat: Error Page Design by Tiamzon

--- src/routes/ErrorCatcher.jsx
+++ src/routes/ErrorCatcher.jsx
@@ -1,26 +1,61 @@
-import { useRouteError } from "react-router-dom"
+import { useRouteError } from "react-router-dom";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 
-const knownErrors = { "DatabaseClosedError": <NoDB /> }
+const knownErrors = {
+    DatabaseClosedError: <NoDB />
+};
+
+const hstyle = {
+    margin: "3vmin",
+    fontFamily: "Century Gothic",
+    textAlign: "center",
+    fontSize: "xxx-large",
+};
+
+const longtextstyle = {
+    color: "white",
+    fontFamily: "Arial",
+    marginInline: "8vw",
+    fontSize: "1.3em",
+    fontWeight: "lighter",
+};
+
+const iconstyle = {
+    fontSize: 100,
+    color: "red",
+    margin: "auto",
+    marginTop: "15%",
+    marginBottom: "0%",
+};
+
 /**
  * @see https://reactrouter.com/en/main/hooks/use-route-error
  */
 export default function ErrorCatcher() {
     const error = useRouteError();
     return <main style={{ color: "white" }}>
-        {knownErrors[error.name]
-            ?? <>
-            {error.name}
-            </>
-        }
-    </main>
+        <ErrorOutlineIcon sx={iconstyle} />
+        <h1 style={hstyle}>Oops!</h1>
+        {knownErrors[error.name] ?? <>{error.name}</>}
+    </main>;
 }
 
 /**
  * @see https://github.com/dexie/Dexie.js/issues/883
- * @returns 
+ * @returns
  */
 export function NoDB() {
     return <>
-        Sorry! The app does not work on Incognito or private browsing mode as it needs to store persistent data on your device.
-    </>;
+        <h3 style={longtextstyle}>
+            The app does not work on Incognito or Private browsing mode as it needs
+            to store persistent data on your device.
+        </h3>
+        <img
+            src="simple-cartoon-ghost-png-30.png"
+            style={{
+                width: "60vmin",
+                margin: "auto"
+            }}
+        />
+    </>
 }
